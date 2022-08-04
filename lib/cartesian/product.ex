5496079873c8fd5ea0d3df71616fae4559dee33e
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-08-04 09:08:24 -0400

    cartesian/1 added

--- lib/cartesian/product.ex
+++ lib/cartesian/product.ex
@@ -8,10 +8,20 @@ defmodule Product do
     |> Enum.reduce(first, fn
       row, acc -> combine(acc, row)
     end)
+    |>IO.inspect()
   end
 
+  def cartesian(args) do
+    [first | rest] = args
+ 
+     rest
+     |> Enum.reduce(first, fn
+       row, acc -> combine(acc, row)
+     end)
+   end
+
   defp combine(row_1, row_2) do
-    for x <- row_1, y <- row_2, do: [x,y]
+    for x <- row_1, y <- row_2, do: List.flatten([x,y])
   end
 
 end
--- test/cartesian_test.exs
+++ test/cartesian_test.exs
@@ -11,17 +11,17 @@ defmodule CartesianTest do
     assert [["1", "a"], ["1", "b"], ["1", "c"], ["2", "a"], ["2", "b"], ["2", "c"], ["3", "a"], ["3", "b"], ["3", "c"]] == out
   end
 
-  # test "takes open ended amount of rows" do
-  #   out = Product.cartesian([["1", "2"], ["a", "b"], ["Z", "Y"]])
-  #   assert [
-  #     [["1", "a", "Z"]],
-  #     [["1", "a"], "Y"],
-  #     [["1", "b"], "Z"],
-  #     [["1", "b"], "Y"],
-  #     [["2", "a"], "Z"],
-  #     [["2", "a"], "Y"],
-  #     [["2", "b"], "Z"],
-  #     [["2", "b"], "Y"]
-  #   ] == out
-  # end
+  test "takes open ended amount of rows" do
+    out = Product.cartesian([["1", "2"], ["a", "b"], ["Z", "Y"]])
+    assert [
+      ["1", "a", "Z"],
+      ["1", "a", "Y"],
+      ["1", "b", "Z"],
+      ["1", "b", "Y"],
+      ["2", "a", "Z"],
+      ["2", "a", "Y"],
+      ["2", "b", "Z"],
+      ["2", "b", "Y"]
+    ] == out
+  end
 end
